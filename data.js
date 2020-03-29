9f1f26ce926669b21acbd799ffb18a4a24dbb925
Armen Grigorian <info@propertystart.com.au>
2020-03-30 08:50:20 +1100

    Version 1

---
+++ data.js
@@ -0,0 +1,270 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-entryliving",
+      "name": "Entry/Living",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -1.588264635846496,
+        "pitch": 0.026812456045703925,
+        "fov": 1.3219339076578436
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.2635580066369556,
+          "pitch": 0.25342973373950883,
+          "rotation": 0,
+          "target": "1-kitchenmeals"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-kitchenmeals",
+      "name": "Kitchen/Meals",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.7260549545987374,
+        "pitch": 0.00019689973043313103,
+        "fov": 1.3219339076578436
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.2053714888145457,
+          "pitch": 0.19645494928511198,
+          "rotation": 0,
+          "target": "2-upstairs-retreat"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-upstairs-retreat",
+      "name": "Upstairs Retreat",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -2.050344067371247,
+        "pitch": 0.12519364267117084,
+        "fov": 1.3219339076578436
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-master-with-en-suite",
+      "name": "Master with en-suite",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 2.1838545784592798,
+        "pitch": -0.04531590835686217,
+        "fov": 1.3219339076578436
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.134305560436074,
+          "pitch": 0.20223070262332676,
+          "rotation": 4.71238898038469,
+          "target": "2-upstairs-retreat"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-bedroom-2",
+      "name": "Bedroom 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -2.7996468473007283,
+        "pitch": 0.012371366972871556,
+        "fov": 1.3219339076578436
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.7770827873007278,
+          "pitch": 0,
+          "rotation": 5.497787143782138,
+          "target": "2-upstairs-retreat"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-bedroom-3",
+      "name": "Bedroom 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -1.1968701473295038,
+        "pitch": 0.21919765422464543,
+        "fov": 1.3219339076578436
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6495298340148494,
+          "pitch": 0.13239863975348243,
+          "rotation": 0.7853981633974483,
+          "target": "2-upstairs-retreat"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-upstairs-bathroom",
+      "name": "Upstairs Bathroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.30810404142349057,
+        "pitch": 0.20278987560690354,
+        "fov": 1.3219339076578436
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.14514970309423,
+          "pitch": 0.22031551453419596,
+          "rotation": 0,
+          "target": "2-upstairs-retreat"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "14 Sandalwood Drive Oakleigh South",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
